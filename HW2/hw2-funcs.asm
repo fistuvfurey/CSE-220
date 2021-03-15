2e5b44c94950b94619d4fc34a060be8e52fe07c6
Aidan Furey <aidan.t.furey@gmail.com>
2021-03-15 14:09:51 -0400

    Finish is_digit func

--- HW2/hw2-funcs.asm
+++ HW2/hw2-funcs.asm
@@ -9,7 +9,19 @@ eval:
   jr $ra
 
 is_digit:
-  jr $ra
+  # $a0 contains the arg of this function.
+  # Check to see if $a0 holds a digit.
+  li		$t1, 48		# $t1 = 48 (ASCII value for '0')
+  blt		$a0, $t1, not_digit	# if $a0, < $t1 then not_digit
+  li		$t1, 57		# $t1 = 58 (ASCII value for one above '9')
+  bge		$a0, $t1, not_digit	# if $a0 >= $t1 then not_digit
+  # If we are here, then the arg is a digit.
+  addi	$v0, $0, 1			# $v0 = $0 + 1 (return 1 since the arg was a digit) 
+  j		skip_not_digit				# jump to skip_not_digit
+  not_digit:
+    addi	$v0, $0, 0			# $v0 = $0 + 0 (return 0 since the arg wasn't a digit)
+  skip_not_digit:
+    jr $ra
 
 stack_push:
   jr $ra
--- HW2/is_digit_test.asm
+++ HW2/is_digit_test.asm
@@ -11,8 +11,14 @@ op_stack : .word 0
 .text
 .globl main
 main:
-
   # add code to call and is_digit function
+  addi	$a0, $0, 'a'			# $a0 = $0 + '2' (initialize arg register to a digit char)  
+  jal is_digit # call is_digit
+  add		$s0, $v0, $0 		# $s0 = $v0 + $0 (save returned value into $s0)
+  
+
+
+  
 
 end:
   # Terminates the program
