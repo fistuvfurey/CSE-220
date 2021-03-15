a1aeee1f3233169c0609f391adab0612760cb5b9
Aidan Furey <aidan.t.furey@gmail.com>
2021-03-15 13:30:20 -0400

    Starter code

---
+++ HW2/hw2-funcs.asm
@@ -0,0 +1,33 @@
+############################ DO NOT CREATE A .data SECTION ############################
+############################ DO NOT CREATE A .data SECTION ############################
+############################ DO NOT CREATE A .data SECTION ############################
+
+############################## Do not .include any files! #############################
+
+.text
+eval:
+  jr $ra
+
+is_digit:
+  jr $ra
+
+stack_push:
+  jr $ra
+
+stack_peek:
+  jr $ra
+
+stack_pop:
+  jr $ra
+
+is_stack_empty:
+  jr $ra
+
+valid_ops:
+  jr $ra
+
+op_precedence:
+  jr $ra
+
+apply_bop:
+  jr $ra
